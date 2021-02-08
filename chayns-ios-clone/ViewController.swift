2616fc0c6636d2e027d8c5ed2e1df4574169bd69
Tim <tim.langner0@gmail.com>
2021-02-08 09:04:45 +0100

    Initial Commit

---
+++ chayns-ios-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chayns-ios-clone
+//
+//  Created by Langner, Tim on 08.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ chayns-ios-cloneTests/chayns_ios_cloneTests.swift
@@ -0,0 +1,33 @@
+//
+//  chayns_ios_cloneTests.swift
+//  chayns-ios-cloneTests
+//
+//  Created by Langner, Tim on 08.02.21.
+//
+
+import XCTest
+@testable import chayns_ios_clone
+
+class chayns_ios_cloneTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
