fcd07a0b7932c41d12a1472038e357a2d78038c5
Tim <tim.langner0@gmail.com>
2021-02-09 17:38:33 +0100

    :construction: Add logo, searchbar and sites container

--- chayns-ios-clone/ViewController.swift
+++ chayns-ios-clone/ViewController.swift
@@ -70,39 +70,47 @@ class ViewController: UIViewController {
         
         // Add profile container to parent view
         self.view.addSubview(profileContainer)
-
+        
         // Create chayns label
         let chaynsLabel:UILabel = UILabel()
         chaynsLabel.text = "chayns"
+        chaynsLabel.textAlignment = .center
         chaynsLabel.textColor = .white
-        chaynsLabel.font = UIFont.systemFont(ofSize: 45)
+        chaynsLabel.font = UIFont.systemFont(ofSize: 45, weight: .bold)
+        chaynsLabel.frame = CGRect(x: 0, y: profileContainer.frame.maxY + 50, width: self.view.frame.width, height: 50)
+        
+        // Add chayns label to parent view
+        self.view.addSubview(chaynsLabel)
 
         // Create search bar
         let searchBar:UISearchBar = UISearchBar()
         searchBar.barStyle = UIBarStyle.black
+        searchBar.placeholder = "Finden"
+        searchBar.frame = CGRect(x: self.view.frame.minX + 25, y: chaynsLabel.frame.maxY + 40, width: self.view.frame.width - 50, height: 40)
+        
+        // Add search bar to parent view
+        self.view.addSubview(searchBar)
 
         // Create sites container
         let sitesContainer:UIView = UIView()
-
-        // Set position for sites container
-        // sitesContainer.frame = CGRect(x: 20, y: 256, width: self.view.frame.width - 20 - 20 /*leftMargin-rightMargin*/, height: self.view.frame.height - (searchBar.frame.maxY) - self.view.frame.maxY)
-
-        sitesContainer.backgroundColor = .blue
+        sitesContainer.frame = CGRect(x: self.view.frame.minX + 45, y: searchBar.frame.maxY + 30, width: searchBar.frame.size.width - 40, height: 450)
+        sitesContainer.layer.borderWidth = 1
+        sitesContainer.layer.borderColor = UIColor.white.cgColor
 
         let campusIcon = UIImage(named: "labs-icon")
 
         for _ in 0...7 {
             // Create a new UIImageView as a container for the image
             let imageView:UIImageView = UIImageView()
-            imageView.contentMode = UIView.ContentMode.scaleAspectFit
-            imageView.frame.size.height = 60
-            imageView.frame.size.width = 60
-            imageView.center = CGPoint(x: 10, y: 10) // self.sitesContainer.center
+            imageView.frame = CGRect(x: 55, y: 30, width: 60, height: 60)
             imageView.image = campusIcon
 
             // Add image to the sites container
             sitesContainer.addSubview(imageView)
         }
+        
+        // Add sites container to parent view
+        self.view.addSubview(sitesContainer)
     }
 }
 
